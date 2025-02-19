c430488d659ed500a1812d8e6fafbeb13b512d57
Benjamin Debeerst <benjamin.debeerst@posteo.de>
2025-02-19 14:14:11 +0100

    Backport CreditOrDebit column class from 0.3.0-dev
    This way the functionality can already be used with beangulp 0.2.0 which
    is currently the latest released version.

--- importers.py
+++ importers.py
@@ -4,6 +4,36 @@ import re
 from beangulp import mimetypes
 from beangulp.importers import csvbase
 
+# Copied from beangulp 0.3.0-dev in order to be able to run on a released version
+# TODO remove and replace with csvbase.CreditOrDebit once 0.3.0 is released
+class CreditOrDebit(csvbase.Column):
+    """Specialized Column for positive and negative amounts on separate columns.
+    Parse and return the amount present in the credit or debit
+    fields. The amount in the debit field is negated before being
+    returned. Only one of the two fields may be populated. The parsing
+    is done as per the Amount column type.
+    Args:
+      credit: Column name or index for amount.
+      debit: Column name or index for negated amount.
+      subs: Dictionary mapping regular expression patterns to
+        replacement strings. Substitutions are performed with
+        re.sub() in the order they are specified.
+      default: Value to return if both fields are empty, if specified.
+    """
+    def __init__(self, credit, debit, subs=None, default=None):
+        super().__init__(credit, debit, default=default)
+        self.subs = subs if subs is not None else {}
+    def parse(self, credit, debit):
+        if credit and debit:
+            raise csvbase.ValueError('credit and debit fields cannot be populated ar the same time')
+        if not credit and not debit:
+            raise csvbase.ValueError('neither credit or debit fields are populated')
+        value = credit if credit else debit
+        for pattern, replacement in self.subs.items():
+            value = re.sub(pattern, replacement, value)
+        parsed = csvbase.decimal.Decimal(value)
+        return parsed if credit else -parsed
+
 class SemicolonCSV(Dialect):
     delimiter = ';'
     quotechar = '"'
@@ -48,7 +78,7 @@ class BPGOImporter(csvbase.Importer):
     Importer for BPGO
     '''
     date = csvbase.Date('Date de comptabilisation', '%d/%m/%Y')
-    amount = csvbase.CreditOrDebit(credit='Credit', debit='Debit', subs = {',': '.', '-': ''})
+    amount = CreditOrDebit(credit='Credit', debit='Debit', subs = {',': '.', '-': ''})
     narration = csvbase.Columns('Libelle operation', 'Informations complementaires', 'Type operation')
 
     def __init__(self, target_account):
